a601934533fd6f2ed01db73d35cbaba85f14e3d5
Niklas.Budimir <4124@htl.rennweg.at>
2017-11-13 11:57:30 +0100

    update

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niklas
+ * Date: 13.11.2017
+ * Time: 11:43
+ */
+
+require "vendor/autoload.php";
+
+use HTL3R\KungFuMovies;
+
+$film1 = new KungFuMovies\Filmausgabe("Bruce Lee - Der Mann mit der Todeskralle", 5, "https://www.youtube.com/watch?v=80wXmIcyZwk");
+
+
+
+
+
+/*
+ * <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <h1>Album DB</h1>
+    <h3>{album1}</h3>
+
+</body>
+</html>
+ */
